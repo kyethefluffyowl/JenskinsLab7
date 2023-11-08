b6f95f3296a6896fe10b807c074900ad77adec4c
Kye Jordan <friendsbacktoback@gmail.com>
2023-11-08 18:06:40 +0800

    Making GumballMachine.php bugged to test jenkins

--- GumballMachine.php
+++ GumballMachine.php
@@ -13,6 +13,7 @@ class GumballMachine {
 	}
 	
 	public function turnWheel() {
-		$this->setGumballs($this->getGumballs()-1);
+		// $this->setGumballs($this->getGumballs()-1); // Working code
+		$this->setGumballs($this->getGumballs()-2) // Buggy code
 	}
 }
